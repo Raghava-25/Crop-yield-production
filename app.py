8a8f5cdc0331c50950007984f8646ba2ca548745
RAGHAVA <159159993+Raghava-25@users.noreply.github.com>
2025-03-05 17:59:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ if st.button('🚜 Predict Crop Yield', key="predict_main"):
 
 
 # Footer
-st.markdown("<p style='text-align:center; color:#888888; margin-top:30px;'>🌱 Powered by RJHD</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align:center; color:#888888; margin-top:30px;'>🌱 Powered by RJHV</p>", unsafe_allow_html=True)
