17e4fce251aa74e4ea9662622f7c6047e8dae613
RAGHAVA <159159993+Raghava-25@users.noreply.github.com>
2025-03-07 12:03:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ st.sidebar.header("🌾 Crop Selection")
 crop = st.sidebar.selectbox("Select Crop", crops)
 
 st.sidebar.header("🌦 Environmental Factors")
-temperature = st.sidebar.slider('Temperature (°C)', 0.0, 30.0, 25.0, step=0.1)
+temperature = st.sidebar.slider('Temperature (°C)', 0.0, 30.0, 50.0, step=0.1)
 humidity = st.sidebar.slider('Humidity (%)', 0.0, 100.0, 40.0, step=0.1)
 soil_moisture = st.sidebar.slider('Soil Moisture (%)', 20.0, 100.0, 20.0, step=0.1)
 area = st.sidebar.number_input('Area (acres)', min_value=0.5, max_value=1000.0, value=4.0, step=0.1)
